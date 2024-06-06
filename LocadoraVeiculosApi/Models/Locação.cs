e9705db7007befdd963aa440c24f75f732b9a547
Lucas Couto <81658256+LucasCouto13@users.noreply.github.com>
2024-06-05 21:08:37 -0300

    Atualizando .gitignore para ignorar arquivos desnecessários e resolver problemas de permissão

--- LocadoraVeiculosApi/Models/Locação.cs
+++ LocadoraVeiculosApi/Models/Locação.cs
@@ -2,5 +2,17 @@
 {
     public class Locação
     {
+        public int Id { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public required string Status { get; set; }
+        public string? Avaliacao { get; set; }
+
+        public Veiculo Veiculo { get; set; }
+        public int VeiculoId { get; set; }
+
+        public Cliente Cliente { get; set; }
+        public int ClienteId { get; set; }
+
     }
 }
--- LocadoraVeiculosApi/Models/Veiculo.cs
+++ LocadoraVeiculosApi/Models/Veiculo.cs
@@ -2,5 +2,12 @@
 {
     public class Veiculo
     {
+        public int Id { get; set; }
+        public required string Placa { get; set; }
+        public int Ano { get; set; }
+        public string? Modelo { get; set; }
+        public string? Marca { get; set; }
+        public string? Sobre { get; set; }
+        public bool Disponivel { get; set; }
     }
 }
